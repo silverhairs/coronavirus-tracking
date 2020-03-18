5211f4081d1a5e8b4f93204933e1fbd565f42c9f
Boris Kayi <boriskayienzo@gmail.com>
2020-03-18 21:21:26 +0200

    Add: Search bar logic

--- lib/screens/countries_screen.dart
+++ lib/screens/countries_screen.dart
@@ -1,87 +1,165 @@
-import 'package:covid/components/search_bar.dart';
-import 'package:covid/constants.dart';
+import 'package:covid/extensions/string_extension.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:covid/constants.dart';
 
-class Countries extends StatelessWidget {
+bool isPinned = false;
+bool isSearching = false;
+
+class Countries extends StatefulWidget {
   Countries({this.countriesList});
   final List countriesList;
-  bool isPinned = false;
+
+  @override
+  _CountriesState createState() => _CountriesState();
+}
+
+class _CountriesState extends State<Countries> {
+  List filteredCountries = [];
+
+  void findCountry(value) {
+    filteredCountries = widget.countriesList
+        .where(
+          (country) => country['country'] == value,
+        )
+        .toList();
+  }
+
+  @override
+  void initState() {
+    filteredCountries = widget.countriesList;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
-        title: Text('Affected Countries'),
+        title: isSearching
+            ? Container(
+                width: double.infinity,
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                decoration: BoxDecoration(
+                  color: kBoxColor,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.search, color: Colors.white),
+                    SizedBox(width: 15),
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration.collapsed(
+                          hintText: 'Search country...',
+                        ),
+                        onChanged: (value) {
+                          setState(() {
+                            value == ''
+                                ? filteredCountries = widget.countriesList
+                                : findCountry(value);
+                          });
+                        },
+                      ),
+                    )
+                  ],
+                ),
+              )
+            : Text('Affected Countries'),
         centerTitle: true,
         backgroundColor: Colors.black,
+        actions: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(right: 8.0),
+            child: IconButton(
+              icon: Hero(
+                child: Icon(isSearching ? Icons.cancel : Icons.search,
+                    color: Colors.white),
+                tag: 'search',
+              ),
+              onPressed: () {
+                setState(() {
+                  isSearching = !isSearching;
+                });
+              },
+            ),
+          )
+        ],
       ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(15),
-            child: SearchBar(),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: countriesList.length,
-              itemBuilder: (context, index) {
-                var country = countriesList[index];
-                return Container(
-                  width: double.infinity,
-                  margin: EdgeInsets.all(15),
-                  padding: EdgeInsets.all(15),
+      body: widget.countriesList == null
+          ? SpinKitFadingCircle(
+              itemBuilder: (BuildContext context, int index) {
+                return DecoratedBox(
                   decoration: BoxDecoration(
-                      color: kBoxColor, borderRadius: kBoxesRadius),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Text(
-                            country['country'].toUpperCase(),
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 22),
-                            textAlign: TextAlign.center,
-                          ),
-                          IconButton(
-                            icon: Icon(
-                              FontAwesomeIcons.thumbtack,
-                              color: isPinned ? Colors.blue : Colors.grey,
-                            ),
-                            onPressed: () {
-                              isPinned = !isPinned;
-                            },
-                          )
-                        ],
-                      ),
-                      SizedBox(height: 5),
-                      Divider(color: Colors.white, thickness: .5),
-                      SizedBox(height: 15),
-                      Text('Total cases: ${country['cases']}',
-                          style: TextStyle(fontSize: 18)),
-                      Text('Today cases: ${country['todayCases']}',
-                          style: TextStyle(fontSize: 18)),
-                      Text('Total deaths: ${country['deaths']}',
-                          style: TextStyle(fontSize: 18)),
-                      Text('Today deaths: ${country['todayDeaths']}',
-                          style: TextStyle(fontSize: 18)),
-                      Text('Recovered: ${country['recovered']}',
-                          style: TextStyle(fontSize: 18)),
-                      Text('In critical state: ${country['critical']}',
-                          style: TextStyle(fontSize: 18)),
-                    ],
+                    color: index.isEven ? Colors.red : Colors.green,
                   ),
                 );
               },
+            )
+          : Column(
+              children: <Widget>[
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: filteredCountries.length,
+                    itemBuilder: (context, index) {
+                      var country = filteredCountries[index];
+                      return Container(
+                        width: double.infinity,
+                        margin: EdgeInsets.all(15),
+                        padding: EdgeInsets.all(15),
+                        decoration: BoxDecoration(
+                            color: kBoxColor, borderRadius: kBoxesRadius),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Text(
+                                  country['country'].toUpperCase(),
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 22),
+                                  textAlign: TextAlign.center,
+                                ),
+                                IconButton(
+                                  icon: Icon(
+                                    FontAwesomeIcons.thumbtack,
+                                    color: isPinned ? Colors.blue : Colors.grey,
+                                  ),
+                                  onPressed: () {
+                                    setState(() {
+                                      isPinned = !isPinned;
+                                    });
+                                  },
+                                )
+                              ],
+                            ),
+                            SizedBox(height: 5),
+                            Divider(color: Colors.white, thickness: .5),
+                            SizedBox(height: 15),
+                            Text('Total cases: ${country['cases']}',
+                                style: TextStyle(fontSize: 18)),
+                            Text('Today cases: ${country['todayCases']}',
+                                style: TextStyle(fontSize: 18)),
+                            Text('Total deaths: ${country['deaths']}',
+                                style: TextStyle(fontSize: 18)),
+                            Text('Today deaths: ${country['todayDeaths']}',
+                                style: TextStyle(fontSize: 18)),
+                            Text('Recovered: ${country['recovered']}',
+                                style: TextStyle(fontSize: 18)),
+                            Text('In critical state: ${country['critical']}',
+                                style: TextStyle(fontSize: 18)),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
-      ),
     );
   }
 }
