fde44362ea5da316bd9063d35c923a9870436e01
Boris Kayi <boriskayienzo@gmail.com>
2020-03-24 01:29:14 +0200

    Add link to WHO website

--- lib/screens/who_advice.dart
+++ lib/screens/who_advice.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class WhoAdvice extends StatelessWidget {
   final bool isDark;
@@ -11,45 +12,53 @@ class WhoAdvice extends StatelessWidget {
       appBar: AppBar(
         title: Text('Protective measures'),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(10.0),
-        child: Container(
-          child: FutureBuilder(
-            future: DefaultAssetBundle.of(context)
-                .loadString("assets/docs/protective_measures.md"),
-            builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-              if (snapshot.hasData) {
-                return Markdown(
-                  data: snapshot.data,
-                  styleSheet: MarkdownStyleSheet(
-                    textAlign: WrapAlignment.start,
-                    p: TextStyle(
-                      fontSize: 16,
-                      color: isDark ? Colors.white : Colors.black,
-                    ),
-                    h2: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: isDark ? Colors.white : Colors.black,
-                    ),
-                    h1: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 25,
-                      color: isDark ? Colors.white : Colors.black,
-                    ),
-                  ),
-                );
-              }
-
-              return Center(
-                child: CircularProgressIndicator(
-                  backgroundColor: Colors.blue,
-                  strokeWidth: 5,
+      body: FutureBuilder(
+        future: DefaultAssetBundle.of(context)
+            .loadString("assets/docs/protective_measures.md"),
+        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+          if (snapshot.hasData) {
+            return Markdown(
+              onTapLink: (url) {
+                launch(url);
+              },
+              data: snapshot.data,
+              styleSheet: MarkdownStyleSheet(
+                textAlign: WrapAlignment.start,
+                p: TextStyle(
+                  fontSize: 16,
+                  color: isDark ? Colors.white : Colors.black,
                 ),
-              );
-            },
-          ),
-        ),
+                h2: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: isDark ? Colors.white : Colors.black,
+                ),
+                h1: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                  color: isDark ? Colors.white : Colors.black,
+                ),
+                h3: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                  color: isDark ? Colors.white : Colors.black,
+                ),
+                a: TextStyle(
+                  decoration: TextDecoration.underline,
+                  color: Colors.blue,
+                ),
+                
+              ),
+            );
+          }
+
+          return Center(
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.blue,
+              strokeWidth: 5,
+            ),
+          );
+        },
       ),
     );
   }
