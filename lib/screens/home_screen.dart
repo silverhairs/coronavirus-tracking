ddb457559df874d864da73390c82d38a50ad04fd
Boris Kayi <boriskayienzo@gmail.com>
2020-03-25 18:43:33 +0200

    Add  drawer to show more features

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -23,6 +23,7 @@ class _HomeState extends State<Home> {
   int numberOfCountries = 0;
   List countries = [];
   bool isDarkTheme = false;
+
   Future getData() async {
     http.Response response = await http.get(allCasesAPI);
     if (response.statusCode >= 200 && response.statusCode <= 299) {
@@ -37,7 +38,7 @@ class _HomeState extends State<Home> {
     }
   }
 
-  Future getCountries() async {
+  Future getCountriesData() async {
     http.Response response = await http.get(affectedCountriesAPI);
     if (response.statusCode >= 200 && response.statusCode <= 299) {
       setState(() {
@@ -52,8 +53,8 @@ class _HomeState extends State<Home> {
   @override
   void initState() {
     getData();
-    getCountries();
-    Timer.periodic(Duration(hours: 3), (Timer t) => getCountries());
+    getCountriesData();
+    Timer.periodic(Duration(hours: 3), (Timer t) => getCountriesData());
     Timer.periodic(Duration(hours: 3), (Timer t) => getData());
     super.initState();
   }
@@ -63,37 +64,74 @@ class _HomeState extends State<Home> {
     ThemeChanger _themeChanger = Provider.of<ThemeChanger>(context);
 
     return Scaffold(
+      appBar: AppBar(
+        leading: Container(),
+        elevation: 0,
+      ),
+      endDrawer: Drawer(
+        elevation: 4,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text(
+                'Settings',
+                style: Theme.of(context)
+                    .textTheme
+                    .headline
+                    .copyWith(color: Colors.white),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.blueAccent,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Features'),
+                  SizedBox(height: 8),
+                  Text('Show on Map', style: Theme.of(context).textTheme.title),
+                  SizedBox(height: 10),
+                  Text('Display'),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text('Change Theme',
+                          style: Theme.of(context).textTheme.title),
+                      SizedBox(width: 20),
+                      Icon(FontAwesomeIcons.sun,
+                          color: Colors.blueGrey, size: 20),
+                      Switch(
+                        activeColor: Colors.blue,
+                        value: isDarkTheme,
+                        onChanged: (value) {
+                          setState(() {
+                            isDarkTheme = !isDarkTheme;
+                            _themeChanger.setTheme(
+                              isDarkTheme ? kDarkTheme : kLightTheme,
+                            );
+                          });
+                        },
+                      ),
+                      Icon(FontAwesomeIcons.moon,
+                          color: Colors.blueGrey, size: 20)
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.only(top:5, left: 10, right: 10, bottom: 10),
+          padding: EdgeInsets.only(top: 5, left: 10, right: 10, bottom: 10),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    IconButton(
-                      icon: Icon(
-                        isDarkTheme
-                            ? FontAwesomeIcons.moon
-                            : FontAwesomeIcons.sun,
-                        color: Colors.blueGrey,
-                        size: 20,
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          isDarkTheme = !isDarkTheme;
-                          _themeChanger.setTheme(
-                            isDarkTheme ? kDarkTheme : kLightTheme,
-                          );
-                        });
-                      },
-                    )
-                  ],
-                ),
-              ),
               Flexible(
                 child: ListView(
                   children: <Widget>[
